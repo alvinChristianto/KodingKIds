7c6cc8a8512045686041a845f044abecc945843f
alvinChristianto <alvinchristianto93@gmail.com>
2024-03-30 14:28:38 +0700

    edit:first mass edit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,14 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        primary: '#042f2e', //safety orange
+
+        secondary: '#d97706', //lovely teal
+      },
       fontFamily: {
-        sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+        inter: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+        sans: ["Poppins", "sans-serif"],
       },
     },
   },
